7dad8d24279fad43aa83fd4e93914c6908a853f7
Rohit Bhure <rohitbhure.cse@gmail.com>
2024-03-08 23:10:05 +0530

    fixing bug

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -78,6 +78,7 @@ const HeroSection = () => {
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
+              loading="lazy"
             />
           </div>
         </motion.div>
